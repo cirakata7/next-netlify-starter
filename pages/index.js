f3a9fbdcc4be16088b5f3bf373fd7c1f0c489656
cirakata7 <56245126+cirakata7@users.noreply.github.com>
2021-12-30 09:53:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="nitishjeet7.ml to nitishjeet7.online" />
+        <Header title="nitishjeet7<del>.ml</del> <br>to<br> nitishjeet7.online" />
 //         <p className="description">
 //           Get started by editing <code>pages/index.js</code>
 //         </p>
